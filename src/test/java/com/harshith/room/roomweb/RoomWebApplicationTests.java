c87a1709da7d9f0b25ececb59b0694944f1c57d3
harshithkamisetty <kamisetty.harshith1@gmail.com>
2021-07-30 11:11:52 +0530

    Sprin boot app

---
+++ src/test/java/com/harshith/room/roomweb/RoomWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshith.room.roomweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
